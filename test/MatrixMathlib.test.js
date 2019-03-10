3269694788a7fba9cd63d4fcead3abad0d2036d1
kareem <kareem.elshahawe@gmail.com>
2019-03-11 01:02:36 +0800

    Rename Matrix -> MatrixMath.js

--- test/MatrixMathlib.test.js
+++ test/MatrixMathlib.test.js
@@ -1,4 +1,4 @@
-const Matrix = require('../lib/Matrix');
+const Matrix = require('../lib/MatrixMath');
 const assert = require('jest').expect;
 describe(`Test Matrix Dimension validation`,()=>{
     it(`Should return true for valid dimensions`,()=>{
@@ -9,5 +9,14 @@ describe(`Test Matrix Dimension validation`,()=>{
     it(`Should return false for Invalid dimensions`,()=>{
         let matrix= new Matrix();
         expect(matrix.isDefined(3,2)).toBeFalsy();
+    });
+});
+
+describe(`Test Matrix dot product`,()=>{
+    it(`Dot product should equal 13`,()=>{
+        let m1Row = [2,3,1];
+        let m2Col = [1,3,2];
+        let matrix = new Matrix();
+        expect(matrix.calcDP(m1Row,m2Col)).toEqual(13);
     });
 });
